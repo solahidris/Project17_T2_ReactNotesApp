10ee8b41666e5e0708e158e6c7268017fb4802e4
solahidris <emailsolah@gmail.com>
2023-06-23 16:27:07 +0900

    added app description

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 function App() {
   return (
     <div className="App">
+      <div className="py-5"></div>
 
-      <div className="container">
-        <h1>Hello, Bootstrap!</h1>
+      <figure class="text-center">
+        <blockquote class="blockquote">
+          <p>A React Notes App</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          users can enter text to each note and
+        </figcaption>
+        <figcaption class="blockquote-footer">
+          save it in local storage,
+        </figcaption>
+        <figcaption class="blockquote-footer">
+          delete an existing note,
+        </figcaption>
+        <figcaption class="blockquote-footer">
+          dynamically search among the notes and
+        </figcaption>
+        <figcaption class="blockquote-footer">add character limit.</figcaption>
         <button className="btn btn-primary">Click Me</button>
-      </div>
+      </figure>
+
+      <div></div>
     </div>
   );
 }
